a3526b11b5482698935e6e51882a4682aaeccd5b
chenpeng <peng1.chen@zqgame.com>
2018-03-14 13:04:00 +0800

    初始化

---
+++ src/main/java/com/zqgame/netty/io/App.java
@@ -0,0 +1,17 @@
+package com.zqgame.netty.io;
+
+/**
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+
+		System.out.println("Hello World!");
+
+
+		System.out.println("坑");
+		System.out.println("坑");
+
+	}
+}
---
+++ src/test/java/com/zqgame/AppTest.java
@@ -0,0 +1,20 @@
+package com.zqgame;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+		extends TestCase {
+
+	@Test
+	public void testApp(){
+		System.out.println("123456");
+
+		assertTrue(true);
+
+	}
+
+}
